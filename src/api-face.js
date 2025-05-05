c6aac750dd6bc970cab02ea8420533da204c9934
genesis <cuneytercel23@gmail.com>
2025-05-05 10:07:54 +0000

    "Update By MindBricks For Architectural Change"

--- src/api-face.js
+++ src/api-face.js
@@ -11,12 +11,549 @@ module.exports = (app) => {
     brand: {
       name: "tickatme",
       image: "https://mindbricks.com/images/logo-light.svg",
-      moduleName: "bff",
+      moduleName: "auth",
     },
     auth: {
       url: authUrl,
     },
-    dataObjects: [],
+    dataObjects: [
+      {
+        name: "User",
+        description:
+          "A data object that stores the user information and handles login settings.",
+        reference: {
+          tableName: "user",
+          properties: [
+            {
+              name: "email",
+              type: "String",
+            },
+
+            {
+              name: "password",
+              type: "String",
+            },
+
+            {
+              name: "fullname",
+              type: "String",
+            },
+
+            {
+              name: "avatar",
+              type: "String",
+            },
+
+            {
+              name: "roleId",
+              type: "String",
+            },
+
+            {
+              name: "emailVerified",
+              type: "Boolean",
+            },
+          ],
+          endpoints: [
+            {
+              isAuth: false,
+              method: "POST",
+              url: "/users",
+              title: "registerUser",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  email: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  password: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  fullname: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  avatar: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  roleId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  emailVerified: {
+                    operator: "eq",
+                    value: "Boolean",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "PATCH",
+              url: "/users/:userId",
+              title: "updateUser",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  password: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  fullname: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  avatar: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "PATCH",
+              url: "/userroles/:userId",
+              title: "updateUserRole",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  roleId: {
+                    operator: "eq",
+                    value: "ID",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "GET",
+              url: "/users/:userId",
+              title: "getUser",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {},
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "GET",
+              url: "/users",
+              title: "listUsers",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {},
+              },
+            },
+          ],
+        },
+      },
+
+      {
+        name: "GivenPermission",
+        description:
+          "A data object that stores the assigment of a specific named permission to a role, usergroup or user for a specific object or for general use.",
+        reference: {
+          tableName: "givenPermission",
+          properties: [
+            {
+              name: "permissionName",
+              type: "String",
+            },
+
+            {
+              name: "roleId",
+              type: "String",
+            },
+
+            {
+              name: "subjectUserId",
+              type: "String",
+            },
+
+            {
+              name: "subjectUserGroupId",
+              type: "String",
+            },
+
+            {
+              name: "objectId",
+              type: "String",
+            },
+
+            {
+              name: "canDo",
+              type: "Boolean",
+            },
+          ],
+          endpoints: [
+            {
+              isAuth: true,
+              method: "POST",
+              url: "/givenpermissions",
+              title: "createGivenPermission",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  permissionName: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  roleId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  subjectUserId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  subjectUserGroupId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  objectId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  canDo: {
+                    operator: "eq",
+                    value: "Boolean",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "POST",
+              url: "/rolepermissions",
+              title: "createRolePermission",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  roleId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  permissionName: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  canDo: {
+                    operator: "eq",
+                    value: "Boolean",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "POST",
+              url: "/userpermissions",
+              title: "createUserPermission",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  subjectUserId: {
+                    operator: "eq",
+                    value: "ID",
+                  },
+                  permissionName: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  canDo: {
+                    operator: "eq",
+                    value: "Boolean",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "POST",
+              url: "/grouppermissions",
+              title: "createGroupPermission",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  subjectUserGroupId: {
+                    operator: "eq",
+                    value: "ID",
+                  },
+                  permissionName: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  canDo: {
+                    operator: "eq",
+                    value: "Boolean",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "POST",
+              url: "/rolegrouppermissions",
+              title: "createPermissionForRoleAndGroup",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  roleId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  subjectUserGroupId: {
+                    operator: "eq",
+                    value: "ID",
+                  },
+                  permissionName: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  canDo: {
+                    operator: "eq",
+                    value: "Boolean",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "POST",
+              url: "/objectpermissions",
+              title: "createObjectPermission",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  objectId: {
+                    operator: "eq",
+                    value: "ID",
+                  },
+                  subjectUserId: {
+                    operator: "eq",
+                    value: "ID",
+                  },
+                  permissionName: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  canDo: {
+                    operator: "eq",
+                    value: "Boolean",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "POST",
+              url: "/objectgrouppermissions",
+              title: "createObjectPermissionForGroup",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  objectId: {
+                    operator: "eq",
+                    value: "ID",
+                  },
+                  subjectUserGroupId: {
+                    operator: "eq",
+                    value: "ID",
+                  },
+                  permissionName: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  canDo: {
+                    operator: "eq",
+                    value: "Boolean",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "POST",
+              url: "/objectrolepermissions",
+              title: "createObjectPermissionForRole",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  objectId: {
+                    operator: "eq",
+                    value: "ID",
+                  },
+                  roleId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  permissionName: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  canDo: {
+                    operator: "eq",
+                    value: "Boolean",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "POST",
+              url: "/objectgrouprolepermissions",
+              title: "createObjectPermissionForRoleAndGroup",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  objectId: {
+                    operator: "eq",
+                    value: "ID",
+                  },
+                  roleId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  subjectUserGroupId: {
+                    operator: "eq",
+                    value: "ID",
+                  },
+                  permissionName: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  canDo: {
+                    operator: "eq",
+                    value: "Boolean",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "PATCH",
+              url: "/givenpermissions/:givenPermissionId",
+              title: "updateGivenPermission",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {
+                  permissionName: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  roleId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  subjectUserId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  subjectUserGroupId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  objectId: {
+                    operator: "eq",
+                    value: "String",
+                  },
+                  canDo: {
+                    operator: "eq",
+                    value: "Boolean",
+                  },
+                },
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "DELETE",
+              url: "/givenpermissions/:givenPermissionId",
+              title: "deleteGivenPermission",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {},
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "GET",
+              url: "/givenpermissions/:givenPermissionId",
+              title: "getGivenPermission",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {},
+              },
+            },
+
+            {
+              isAuth: true,
+              method: "GET",
+              url: "/givenpermissions",
+              title: "listGivenPermissions",
+              query: [],
+
+              body: {
+                type: "json",
+                content: {},
+              },
+            },
+          ],
+        },
+      },
+    ],
   };
 
   inject(app, config);
